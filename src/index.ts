fa3786effd52582d78a011ffbeafe04b1c69294d
EngelberAmaya <engelberamaya@gmail.com>
2023-06-19 19:55:21 -0400

    pluck

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { fromEvent, map, range } from "rxjs";
+import { fromEvent, map, pluck, range } from "rxjs";
 
 // range(1,5).pipe(
 //     map<number, string>(val => (val * 10).toString())
@@ -9,6 +9,12 @@ const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
 
 const keyupCode$ = keyup$.pipe(
     map(event => event.code)
-)
+);
 
+const keyupPluck$ = keyup$.pipe(
+   pluck('target', 'baseURI')
+);
+
+keyup$.subscribe(console.log);
 keyupCode$.subscribe( code => console.log('map', code));
+keyupPluck$.subscribe( code => console.log('pluck', code));
