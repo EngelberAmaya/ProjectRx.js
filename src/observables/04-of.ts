8cf3cac7386a28dfa1fcc001470bcbe5e4b97772
EngelberAmaya <engelberamaya@gmail.com>
2023-06-19 14:16:35 -0400

    of

---
+++ src/observables/04-of.ts
@@ -0,0 +1,9 @@
+import { of } from "rxjs";
+
+const obs$ = of(1,2,3,4,5,6);
+
+obs$.subscribe(
+    next => console.log('next', next),
+    null,
+    () => console.log('Terminamos la secuencia')
+)
