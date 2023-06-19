c2cb18d959b3e9dde407deb97903c6b3f8985ccd
EngelberAmaya <engelberamaya@gmail.com>
2023-06-19 11:12:28 -0400

    Ending observable in chain

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,7 @@
-import {Observable, Observer} from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
 const observer: Observer<any> = {
     next: value => console.log('next', value),
     error: error =>console.warn('error:', error),
     complete: () => console.info('completado')
 }
-
-const intervalo$ = new Observable<number>(subs => {
-
-    let count = 0;
-    const interval = setInterval(() => {
-        count++;
-        subs.next(count);
-        console.log(count);
-    }, 1000);
-
-    return () => {
-        clearInterval(interval);
-        console.log('intérvalo destruido');
-    }
-})
-
-const subs1 = intervalo$.subscribe();
-const subs2 = intervalo$.subscribe();
-const subs3 = intervalo$.subscribe();
-
-
-setTimeout(() => {
-    subs1.unsubscribe();
-    subs2.unsubscribe();
-    subs3.unsubscribe();
-
-    console.log('Completado timeout');
-
-}, 3000)
